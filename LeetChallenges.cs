97635ce54a5b0224b13f6c4880dfd2e3a161ebaf
anothermarkus <anothermarcus@hotmail.com>
2025-08-12 10:18:18 -0500

    Search Range

--- LeetChallenges.cs
+++ LeetChallenges.cs
@@ -187,6 +187,39 @@ public class QNode
 
     static class LeetChallenges
     {
+
+        public static int[] SearchRange(int[] nums, int target) {
+            int first = FindEnd(nums, target, true);
+            int last = FindEnd(nums, target, false);
+            return new int[] { first, last };
+        }
+        private static int FindEnd(int[] nums, int target, bool searchleft) {
+            int left = 0;
+            int right = nums.Length - 1;
+            int bound = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    bound = mid;
+                    if (searchleft) {
+                        // keep looking left
+                        right = mid - 1; 
+                    } else {
+                        // keep looking right
+                        left = mid + 1;  
+                    }
+                }
+                else if (nums[mid] < target) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            return bound;
+        }
         
         public static int FindPeakElement(int[] nums) {
         int left = 0;
